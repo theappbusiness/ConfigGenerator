5b6a4449823c956e8a43880de85b79c4809132a3
Sam Dods <sam@theappbusiness.com>
2015-09-29 13:41:42 +0100

    Initial Commit

---
+++ configen/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  configen
+//
+//  Created by Sam Dods on 29/09/2015.
+//  Copyright © 2015 The App Business. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
